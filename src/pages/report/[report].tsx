6783864e7b48dd9eca54706ccfe36a9911b3563e
Serge <sergesoroka@gmail.com>
2023-10-17 16:56:41 +0300

    Reports: single page

---
+++ src/pages/report/[report].tsx
@@ -0,0 +1,78 @@
+// @ts-nocheck
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import Image from "next/image";
+import useSWR from "swr";
+
+import CountryList from "@/components/CountryList/CountryList";
+
+import { fetcher } from "../../../lib/fetcher";
+import styles from "../../styles/Home.module.css";
+
+function ReportPage() {
+  const [country, setCountry] = useState("");
+  const [media, setMedia] = useState("all");
+  const router = useRouter();
+  const { locale } = router;
+
+  const { data, error } = useSWR(
+    `https://vox-dashboard.ra-devs.tech/api/pages?lang=${locale}`,
+    fetcher
+  );
+
+  const reportDataRender =
+    data &&
+    data.data.map((item, i) => {
+      if (router.query.report == item.date) {
+        return (
+          <div
+            key={item.id}
+            style={{ width: "100%", margin: "0 auto" }}
+            className="reports"
+          >
+            <h1 style={{ textAlign: "center", textTransform: "uppercase" }}>
+              {item.title}
+            </h1>{" "}
+            <p className={styles.reportsAuthor}>{item.author}</p>
+            <p className={styles.reportsLead}>{item.lead}</p>
+            <Image
+              src="https://voxukraine.org/wp-content/uploads/2023/08/Propaganda-diary.png"
+              width={922}
+              height={518}
+              alt="Picture of the author"
+            />
+            <p className={styles.reportsDisclaimer}>{item.disclaimer}</p>
+            <CountryList
+              country={country}
+              setCountry={setCountry}
+              setMedia={setMedia}
+            />
+            <hr
+              style={{
+                height: "2px",
+                background: "#FF2618",
+                border: "none",
+                width: "100%",
+                margin: "2rem 0",
+              }}
+            />
+            <p className={styles.reportsContent}>{item.content[country]}</p>
+          </div>
+        );
+      }
+    });
+
+  return (
+    <div className={styles.mainLeft}>
+      <Head>
+        <title>Propaganda Diary | Report </title>
+        <meta name="description" content="Monthy Report" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {reportDataRender}
+    </div>
+  );
+}
+
+export default ReportPage;
